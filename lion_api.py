5570d4e84862113dcd3b2a5575069d6966e755c7
mohamadrezaeii1999-eng <mohamad.rezaeii1999@gmail.com>
2026-09-04 17:11:25 +0330

    Import requests for cTrader token

--- lion_api.py
+++ lion_api.py
@@ -1,6 +1,7 @@
 import os
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from flask import Flask, jsonify, request, redirect
+import requests
 from flask_cors import CORS
 
 from lion_engine_v37 import analyze
